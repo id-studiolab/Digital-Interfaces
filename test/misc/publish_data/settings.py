6496b1eda4427eb5dc6d2f4c28c76f746a37996d
MartiniMartin <MartiniMartin@users.noreply.github.com>
2026-02-24 12:58:37 +0000

    deploy: 2d7466841fb7c2cc8d8b564a734c8bf660328815

--- test/misc/publish_data/settings.py
+++
@@ -1,7 +0,0 @@
-settings = {
-    "ssid": "great interactive reef",
-    "username" : "",
-    "password": "",
-    "broker": "sciencecentre-tudelft.cloud.shiftr.io",
-    "client_id": "spirit", #your team name
-}
