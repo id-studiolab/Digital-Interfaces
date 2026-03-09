824df455eb39e16d83783147e49ee7253e7ce221
Jerzeek <Jerzeek@users.noreply.github.com>
2026-03-09 09:20:34 +0000

    deploy: d8eba440eb9251e745deb87b06ab3cbaae823ec8

---
+++ test/misc/publish_data/settings.py
@@ -0,0 +1,7 @@
+settings = {
+    "ssid": "great interactive reef",
+    "username" : "",
+    "password": "",
+    "broker": "sciencecentre-tudelft.cloud.shiftr.io",
+    "client_id": "spirit", #your team name
+}
